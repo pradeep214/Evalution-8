b3970fdaa1cfce1ab1e73bc35fd29fcf17a5cb79
Pradeep Dhakad <dhakadpradeep39@gmail.com>
2024-04-14 16:04:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Todolist from './Components/Todolist';
+
+function App() {
+  return (
+    <div className="App">
+  <h1>
+   Todo-List 
+  </h1>
+  <Todolist />
+    </div>
+  );
+}
+
+export default App;
